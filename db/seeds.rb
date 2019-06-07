60e8114d973e09db0a6285579321868e81b31378
nguyenvanhung1995 <ghunx1@gmail.com>
2019-06-07 16:48:58 +0700

    Task100: Index, show user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,13 @@
   name = Faker::Name.name
   Category.create!(name: name)
 end
+
+20.times do |n|
+  User.create!(
+    name:  Faker::Name.name,
+    birthday: Faker::Date.birthday(18, 65),
+    address: Faker::Address.full_address,
+    email: Faker::Internet.email,
+    role:"user",
+    password: "password")
+end

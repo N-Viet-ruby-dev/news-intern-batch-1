ef5e370bc59261362a263235b6fda6e0278017a9
nguyenvanhung1995 <ghunx1@gmail.com>
2019-06-18 08:08:36 +0700

    Task119 : Show posts details

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,8 @@ class PostsController < ApplicationController
     @q = Post.ransack(params[:q])
     @post_results = @q.result(distinct: true).page(params[:page]).per(9)
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
